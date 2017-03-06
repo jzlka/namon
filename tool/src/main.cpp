f04ab7c026532d43414021400d9be43bc552f850
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-06 17:48:14 +0100

    Tabs converted to spaces

--- tool/src/main.cpp
+++ tool/src/main.cpp
@@ -1,21 +1,21 @@
 /** 
- *  @file		main.cpp
- *	@brief		Network Traffic Capturing With Application Tags
+ *  @file       main.cpp
+ *  @brief      Network Traffic Capturing With Application Tags
  *  @details    Bachelor's Thesis, FIT VUT Brno
- *	@author		Jozef Zuzelka (xzuzel00)
- *	Mail:		xzuzel00@stud.fit.vutbr.cz
- *	Created:	18.02.2017 08:03
- *	Edited:		06.03.2017 17:00
- * 	Version:	1.0.0
- *	g++:		Apple LLVM version 8.0.0 (clang-800.0.42.1)
- *	@todo       change tool name
- *	@todo       change default tool name in <char *oFilename>
+ *  @author     Jozef Zuzelka (xzuzel00)
+ *  Mail:       xzuzel00@stud.fit.vutbr.cz
+ *  Created:    18.02.2017 08:03
+ *  Edited:     06.03.2017 17:48
+ *  Version:    1.0.0
+ *  g++:        Apple LLVM version 8.0.0 (clang-800.0.42.1)
+ *  @todo       change tool name
+ *  @todo       change default tool name in <char *oFilename>
  */
 
 #include <iostream>             //  EXIT_*, cout, cerr
 #include <getopt.h>             //  getopt_long()
 #if defined(__linux__)
-#include <signal.h>		//  signal(), SIGINT, SIGTERM, SIGABRT, SIGSEGV
+#include <signal.h>     //  signal(), SIGINT, SIGTERM, SIGABRT, SIGSEGV
 #endif
 #include "capturing.hpp"        //  startCapture()
 #include "debug.hpp"
@@ -41,7 +41,7 @@ int main (int argc, char *argv[])
 {
 /*
 #ifdef WIN32
-	SetConsoleCtrlHandler((PHANDLER_ROUTINE)signalHandler, TRUE);
+    SetConsoleCtrlHandler((PHANDLER_ROUTINE)signalHandler, TRUE);
 #else
 */
     signal(SIGINT, signalHandler);      signal(SIGTERM, signalHandler);
@@ -53,18 +53,18 @@ int main (int argc, char *argv[])
     char *oFilename = "tool_capturedTraffic.pcapng"; //TODO zmenit tool za nazov nastroja
 
     int optionIndex = 0;
-	char opt = 0;
-	while((opt = getopt_long(argc, argv, "i:w:h", longopts, &optionIndex)) != -1)
-	{
-		switch (opt)
-		{
-			case 0:	                    break;
-			case 'i':   dev = optarg;   break;
-			case 'w':   oFilename = optarg; break;
-			case 'h':   printUsage();   return EXIT_SUCCESS;
-			default:	printUsage();	return EXIT_FAILURE;
-		}
-	}
+    char opt = 0;
+    while((opt = getopt_long(argc, argv, "i:w:h", longopts, &optionIndex)) != -1)
+    {
+        switch (opt)
+        {
+            case 0:                     break;
+            case 'i':   dev = optarg;   break;
+            case 'w':   oFilename = optarg; break;
+            case 'h':   printUsage();   return EXIT_SUCCESS;
+            default:    printUsage();   return EXIT_FAILURE;
+        }
+    }
 
     return startCapture(oFilename);
 }
