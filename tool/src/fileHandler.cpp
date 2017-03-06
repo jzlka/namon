f998151b1c3f0b5023ca2b4fbc1cc231093ad12e
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-06 17:46:51 +0100

    Tabs converted to spaces

--- tool/src/fileHandler.cpp
+++ tool/src/fileHandler.cpp
@@ -1,15 +1,15 @@
 /** 
- *  @file		fileHandler.cpp
- *	@brief		Network Traffic Capturing With Application Tags
+ *  @file       fileHandler.cpp
+ *  @brief      Network Traffic Capturing With Application Tags
  *  @details    Bachelor's Thesis, FIT VUT Brno
- *	@author		Jozef Zuzelka (xzuzel00)
- *	Mail:		xzuzel00@stud.fit.vutbr.cz
- *	Created:	06.03.2017 14:51
- *	Edited:		06.03.2017 17:29
- * 	Version:	1.0.0
- *	g++:		Apple LLVM version 8.0.0 (clang-800.0.42.1)
- *	@bug
- *	@todo
+ *  @author     Jozef Zuzelka (xzuzel00)
+ *  Mail:       xzuzel00@stud.fit.vutbr.cz
+ *  Created:    06.03.2017 14:51
+ *  Edited:     06.03.2017 17:45
+ *  Version:    1.0.0
+ *  g++:        Apple LLVM version 8.0.0 (clang-800.0.42.1)
+ *  @bug
+ *  @todo
  */
 
 
