5b09d2bc8126ac69825ff7a609ff5d5dec3fdd68
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-06 17:37:09 +0100

    Tabs converted to spaces

--- src/capturing.cpp
+++ src/capturing.cpp
@@ -1,13 +1,13 @@
 /** 
  *  @file		capturing.cpp
- *	@brief		Network Traffic Capturing With Application Tags
+ *  @brief      Network Traffic Capturing With Application Tags
  *  @details    Bachelor's Thesis, FIT VUT Brno
- *	@author		Jozef Zuzelka (xzuzel00)
- *	Mail:		xzuzel00@stud.fit.vutbr.cz
- *	Created:	18.02.2017 22:45
- *	Edited:		06.03.2017 17:29
- * 	Version:	1.0.0
- *	g++:		Apple LLVM version 8.0.0 (clang-800.0.42.1)
+ *  @author     Jozef Zuzelka (xzuzel00)
+ *  Mail:       xzuzel00@stud.fit.vutbr.cz
+ *  Created:    18.02.2017 22:45
+ *  Edited:		06.03.2017 17:36
+ *  Version:    1.0.0
+ *  g++:        Apple LLVM version 8.0.0 (clang-800.0.42.1)
  */
 
 #include <iostream>             //  cerr, endl
