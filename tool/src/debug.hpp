916f1b930c4e724122f5720ecf79c38d93cc7eca
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-11 03:24:47 +0100

    Added function for printing arrays

--- tool/src/debug.hpp
+++ tool/src/debug.hpp
@@ -5,7 +5,7 @@
  *  @author     Jozef Zuzelka (xzuzel00)
  *  Mail:       xzuzel00@stud.fit.vutbr.cz
  *  Created:    26.09.2016 23:59
- *  Edited:     10.03.2017 04:13
+ *  Edited:     11.03.2017 03:23
  *  g++:        Apple LLVM version 8.0.0 (clang-800.0.42.1)
  */
 
@@ -29,8 +29,20 @@ enum class LogLevel : uint8_t
 extern LogLevel generalLogLevel;
 const char * const msgPrefix[] = { "[II]", "[WW]", "[EE]", "" };
 
+inline void printArray(const unsigned char *bitArray, const unsigned int dataSize)
+{
+    std::cerr << "Data (" << dataSize << "): " ;
+    for (unsigned int i=0; i != dataSize; i++)
+        std::cerr << std::hex << (bitArray[i]>>4) << (bitArray[i]&0x0f);
+    std::cerr << std::endl;
+}
+
+
 
 #ifdef DEBUG_BUILD
+
+
+#define D_ARRAY(bitArray, dataSize) printArray(bitArray, dataSize)
 
 #define D(...) \
     do { \
@@ -38,6 +50,7 @@ const char * const msgPrefix[] = { "[II]", "[WW]", "[EE]", "" };
     std::cerr << "DEBUG: " << __FILE__ << ":" << __LINE__ << ":<" << RED << __func__ <<  CLR << ">: "; \
     std::cerr << __VA_ARGS__ << std::endl; \
     } while (0)
+
 template <typename ... Ts>
 void log(LogLevel ll, Ts&&... args)
 {
@@ -49,11 +62,14 @@ void log(LogLevel ll, Ts&&... args)
     }
 }
 
+
 #else   //  DEBUG_BUILD
 
+
+#define D_ARRAY(x,y)    
 #define D(...)
 template <typename ... Ts>
 void log(Ts&&... ) {}
 
-#endif  // DEBUG_BUILD
 
+#endif  // DEBUG_BUILD
