7d9246a5696d434c2282f49b808eb3efdf9c669a
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-16 04:06:29 +0100

    Netflow inline functions fix

--- src/netflow.cpp
+++ src/netflow.cpp
@@ -4,22 +4,13 @@
  *  @author     Jozef Zuzelka (xzuzel00)
  *  Mail:       xzuzel00@stud.fit.vutbr.cz
  *  Created:    15.03.2017 23:27
- *  Edited:     16.03.2017 00:46
+ *  Edited:     16.03.2017 04:05
  *  Version:    1.0.0
  */
 
 #include "netflow.hpp"
 
 
-
-extern const char * g_dev;
-
-
-Netflow::Netflow(const char *intf) : interface(intf)
-{}
-
-Netflow::Netflow() : interface(g_dev)
-{}
 
 Netflow::~Netflow()                                  
 { 
@@ -35,105 +26,6 @@ Netflow::~Netflow()
     }
 }
 
-inline Directions Netflow::getDir()
-{ 
-    return dir; 
-}
-
-inline void Netflow::setDir(Directions d)
-{ 
-    dir = d; 
-}
-
-inline unsigned char Netflow::getIpVersion()
-{ 
-    return ipVersion; 
-}
-
-inline void Netflow::setIpVersion(unsigned char ipV)        
-{ 
-    ipVersion = ipV; 
-}
-
-inline void * Netflow::getSrcIp()
-{ 
-    return srcIp;  
-}
-
-inline void Netflow::setSrcIp(void *newIp)
-{ 
-    srcIp = newIp; 
-}
-
-inline void * Netflow::getDstIp()
-{ 
-    return dstIp;  
-}
-
-inline void Netflow::setDstIp(void *newIp)
-{ 
-    dstIp = newIp; 
-}
-
-inline unsigned short Netflow::getSrcPort()
-{ 
-    return srcPort;    
-}
-
-inline void Netflow::setSrcPort(unsigned short newPort)
-{ 
-    srcPort = newPort; 
-}
-
-inline unsigned short Netflow::getDstPort()
-{ 
-    return dstPort;    
-}
-
-inline void Netflow::setDstPort(unsigned short newPort)
-{ 
-    dstPort = newPort; 
-}
-
-inline unsigned char Netflow::getProto()
-{ 
-    return proto;    
-}
-
-inline void Netflow::setProto(unsigned char newProto)
-{ 
-    proto = newProto; 
-}
-
-inline const char * Netflow::getInterface()
-{ 
-    return interface; 
-}
-
-inline void Netflow::setInterface(char *newInt)
-{ 
-    interface = newInt; 
-}
-
-inline int Netflow::getStartTime()
-{ 
-    return startTime;    
-}
-
-inline void Netflow::setStartTime(long newTime)
-{ 
-    startTime = newTime; 
-}
-
-inline int Netflow::getEndTime()
-{ 
-    return endTime; 
-}
-
-inline void Netflow::setEndTime(long newTime)
-{ 
-    endTime = newTime; 
-}
 
 bool Netflow::operator==(const Netflow& other) const
 {
--- src/netflow.hpp
+++ src/netflow.hpp
@@ -4,7 +4,7 @@
  *  @author     Jozef Zuzelka (xzuzel00)
  *  Mail:       xzuzel00@stud.fit.vutbr.cz
  *  Created:    26.02.2017 23:13
- *  Edited:     16.03.2017 03:48
+ *  Edited:     16.03.2017 04:05
  *  Version:    1.0.0
  */
 
@@ -14,6 +14,8 @@
 #include <netinet/in.h>     //  in_addr, in6_addr
 
 
+
+extern const char * g_dev;
 
 /*!
  * An enum representing packet flow direction
@@ -53,12 +55,12 @@ public:
     /*!
      * @brief Defalut constructor that sets interface to #g_dev
      */
-    Netflow();
+    Netflow(const char *intf) : interface(intf) {}
     /*!
      * @brief       Constructor that sets interface to the parameter
      * @param[in]   intf     Interface used to capture packets
      */
-    Netflow(const char *intf);
+    Netflow() : interface(g_dev) {}
     /*!
      * @brief   Destructor cleans memory pointed by #Netflow::srcIP and #Netflow::dstIp pointers
      */
@@ -67,27 +69,27 @@ public:
      * @brief   Get method for #Netflow::dir
      * @return  Packet direction
      */
-    Directions getDir();
+    Directions getDir()                     { return dir; }
     /*! 
      * @brief       Set method for #Netflow::dir
      * @param[in]   d         Packet direction
      */
-    void setDir(Directions d);
+    void setDir(Directions d)               { dir = d; }
     /*! 
      * @brief   Get method for #Netflow::ipVersion
      * @return  IP header version
      */
-    unsigned char getIpVersion();
+    unsigned char getIpVersion()            { return ipVersion; }
     /*! 
      * @brief       Set method for #Netflow::ipVersion
      * @param[in]   ipV       IP header version
      */
-    void setIpVersion(unsigned char ipV);
+    void setIpVersion(unsigned char ipV)    { ipVersion = ipV; }
     /*! 
      * @brief   Get method for #Netflow::srcIp
      * @return  Pointer to source IP structure
      */
-    void *getSrcIp();
+    void * getSrcIp()                       { return srcIp; }
     /*! 
      * @brief       Set method for #Netflow::srcIp
      * @pre         newIp must point to a valid in*_addr structure
@@ -95,12 +97,12 @@ public:
      *              Then it will be freed in destructor.
      * @param[in]   newIp     Source IP structure pointer
      */
-    void setSrcIp(void *newIp);
+    void setSrcIp(void *newIp)              { srcIp = newIp; }
     /*! 
      * @brief   Get method for #Netflow::dstIp
      * @return  Pointer to destination IP structure
      */
-    void *getDstIp();
+    void * getDstIp()                       { return dstIp; }
     /*! 
      * @brief       Set method for #Netflow::dstIp
      * @pre         newIp must point to a valid in*_addr structure.
@@ -108,67 +110,67 @@ public:
      *              Then it will be freed in destructor.
      * @param[in]   newIp     Destination IP structure pointer
      */
-    void setDstIp(void *newIp);
+    void setDstIp(void *newIp)              { dstIp = newIp; }
     /*! 
      * @brief   Get method for #Netflow::srcPort
      * @return  Source port
      */
-    unsigned short getSrcPort();
+    unsigned short getSrcPort()             { return srcPort; }
     /*! 
      * @brief       Set method for #Netflow::srcPort
      * @param[in]   newPort   Source port
      */
-    void setSrcPort(unsigned short newPort);
+    void setSrcPort(unsigned short newPort) { srcPort = newPort; }
     /*! 
      * @brief   Get method for #Netflow::dstPort
      * @return  Destination port
      */
-    inline unsigned short getDstPort();
+    unsigned short getDstPort()             { return dstPort; }
     /*! 
      * @brief       Set method for #Netflow::dstPort
      * param[in]    newPort   Destination port
      */
-    void setDstPort(unsigned short newPort);
+    void setDstPort(unsigned short newPort) { dstPort = newPort; }
     /*! 
      * @brief   Get method for #Netflow::proto
      * @return  Layer 4 protocol
      */
-    unsigned char getProto();
+    unsigned char getProto()                { return proto; }
     /*! 
      * @brief       Set method for #Netflow::proto
      * @param[in]   newProto  Layer 4 protocol
      */
-    void setProto(unsigned char newProto);
+    void setProto(unsigned char newProto)   { proto = newProto; }
     /*! 
      * @brief   Get method for #Netflow::interface
      * @return  Interface which was used to capture packets
      */
-    const char *getInterface();
+    const char *getInterface()              { return interface; }
     /*! 
      * @brief       Set method for #Netflow::interface
      * @param[in]   newInt    Interface which was used to capture packets
      */
-    void setInterface(char *newInt);
+    void setInterface(char *newInt)         { interface = newInt; }
     /*! 
      * @brief   Get method for #Netflow::startTime
      * @return  Time of the first packet which belongs to this netflow
      */
-    int getStartTime();
+    int getStartTime()                      { return startTime; }
     /*! 
      * @brief       Set method for #Netflow::startTime
      * @param[in]   newTime   Time of the first packet which belongs to this netflow
      */
-    void setStartTime(long newTime);
+    void setStartTime(long newTime)         { startTime = newTime; }
     /*! 
      * @brief   Get method for #Netflow::endTime
      * @return  Time of the last packet which belongs to this netflow
      */
-    int getEndTime();
+    int getEndTime()                        { return endTime; }
     /*! 
      * @brief       Set method for #Netflow::endTime
      * @param[in]   newTime   Time of the last packet which belongs to this netflow
      */
-    void setEndTime(long newTime);
+    void setEndTime(long newTime)           { endTime = newTime; }
     /*!
      * @brief   Overloaded equality operator
      * @details Compares just netflow relevant variables
