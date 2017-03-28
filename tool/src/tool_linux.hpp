d83bc6ca32b2ee4e906aa458562521e2b5d9485f
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-28 03:56:13 +0200

    Inode searching implemented (IPv4 only)

--- tool/src/tool_linux.hpp
+++ tool/src/tool_linux.hpp
@@ -1,18 +1,21 @@
-/** 
+/**
  *  @file       tool_linux.hpp
  *  @brief      Determining applications and their sockets on Linux
  *  @author     Jozef Zuzelka <xzuzel00@stud.fit.vutbr.cz>
  *  @date
  *   - Created: 18.02.2017 22:55
- *   - Edited:  27.03.2017 00:15
+ *   - Edited:  28.03.2017 02:30
  *  @todo       rename file
  */
 
 #pragma once
 
+#include <fstream>          //  ifstream
 #include "netflow.hpp"      //  Netflow
 #include "cache.hpp"        //  Cache
 
 
-int determineApp (Netflow *n);
+TEntry&& determineApp(Netflow *n);
+unsigned int getInodeIpv4(Netflow *n);
+unsigned int getInodeIpv6(Netflow *n);
 void initCache(Cache *c);
