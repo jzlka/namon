a342bb30a85ccb716b6745662feba59c50f02839
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-29 18:18:37 +0200

    Char string to integer converting function

---
+++ src/utils.cpp
@@ -0,0 +1,23 @@
+/**
+ *  @file       utils.cpp
+ *  @brief      Small useful functions
+ *  @author     Jozef Zuzelka <xzuzel00@stud.fit.vutbr.cz>
+ *  @date
+ *   - Created: 28.03.2017 14:14
+ *   - Edited:  28.03.2017 14:45
+ */
+
+#include <cctype>       //  isdigit()
+
+
+
+int chToInt(char *str, int &res)
+{
+    for(res = 0; *str; str++)
+    {
+        if (!isdigit(*str))
+            return 1;
+        res = res*10 + (*str - '0');
+    }
+    return 0;
+}
