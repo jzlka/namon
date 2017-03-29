53d1e8289fa39e63582d9d2b2cd17129798c14ec
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-03-29 18:24:36 +0200

    Global vector of interface IPs and determining application on Linux

--- src/tool_linux.hpp
+++ src/tool_linux.hpp
@@ -4,7 +4,7 @@
  *  @author     Jozef Zuzelka <xzuzel00@stud.fit.vutbr.cz>
  *  @date
  *   - Created: 18.02.2017 22:55
- *   - Edited:  28.03.2017 02:30
+ *   - Edited:  28.03.2017 21:47
  *  @todo       rename file
  */
 
@@ -15,7 +15,7 @@
 #include "cache.hpp"        //  Cache
 
 
-TEntry&& determineApp(Netflow *n);
-unsigned int getInodeIpv4(Netflow *n);
-unsigned int getInodeIpv6(Netflow *n);
-void initCache(Cache *c);
+int determineApp(Netflow *n, TEntry &e);
+int getInodeIpv4(Netflow *n);
+int getInodeIpv6(Netflow *n);
+int getApp(const int inode, string &appname);
