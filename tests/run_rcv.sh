9dad40aaa34c8ad8ea6acf985b0f7fea25cddbf3
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2017-05-25 17:36:37 +0200

    Deleted files

--- tests/run_rcv.sh
+++
@@ -1,9 +0,0 @@
-#bin/sh
-
-for i in {0..4}; do
-	echo "Test #$i"
-	./udp_server 12345 &
-	PID=$!
-	sleep 8
-	kill -2 $PID
-done
--- tests/run_serv_period.sh
+++
@@ -1,8 +0,0 @@
-#bin/sh
-
-while true; do
-	./udp_server 12345 &
-	PID=$!
-	sleep 8
-	kill -2 $PID
-done
